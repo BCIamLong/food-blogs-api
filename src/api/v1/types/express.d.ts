8f0a20f46ea698e4dda69f68298780fb4954c52c
longhoang <longkai21112002@gmail.com>
2025-03-28 10:29:52 +0700

    fix: can't compiple the .d.ts file with ts-node(in tsc it's normal)

--- src/api/v1/types/express.d.ts
+++ src/api/v1/types/express.d.ts
@@ -1,5 +1,10 @@
 import { User } from "../interfaces";
 
+// * this is the way we can extend the request type in the @types/express (package we already install contains types in express to support develop the app easier)
+// * we can also create our own @types/express by create the @types folder and in here create express.d.ts and create our type
+// * but since express provide us @types/express package we just download and if needed we can custom it
+// * we also can custom it with interface just create the custom request and extends Request type
+
 declare global {
   namespace Express {
     interface Request {
